8d40ffba3ba5f7b64fce4c9865c0346f897321ae
= <awesomepranshu@gmail.com>
2022-11-04 10:54:52 +0530

    Fixed config issues

--- rollup.config.js
+++ rollup.config.js
@@ -12,6 +12,7 @@ import json from "@rollup/plugin-json";
 export default [
   {
     input: "src/index.tsx",
+    external: ["react", "react-dom"],
     output: [
       {
         file: "dist/cjs/index.js",
